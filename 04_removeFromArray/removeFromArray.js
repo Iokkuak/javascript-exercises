23f0566de347df8937387416990ab076415b1b08
iokkuak <iokkuak@gmail.com>
2024-07-26 20:50:32 +0200

    exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    let newArray = [];
+
+    array.forEach(element => {
+        if(!args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
+let a = removeFromArray([1,2,3], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
