538be380ed9b39f7e602050615b3000bfbea773a
iokkuak <iokkuak@gmail.com>
2024-07-26 21:11:43 +0200

    exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if(!Number.isInteger(from) || !Number.isInteger(to)) return "ERROR";
+    if(from < 0 || to < 0) return "ERROR";
 
+    if(to < from) {
+        let aux = to;
+        to = from;
+        from = aux;
+    }
+
+    let sum = 0;
+    for(let i = from; i <= to; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
