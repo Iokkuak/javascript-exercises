c273acedcf84a58107a0ce582dbbfdb99567af2c
iokkuak <iokkuak@gmail.com>
2024-07-28 19:10:06 +0200

    exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const alphaNumeric = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789";
 
-};
+const palindromes = function (string) {
+    string = string.
+        toLowerCase().
+        split('').
+        filter(char => alphaNumeric.includes(char)).
+        join('');
+
+    const reversedString = string.
+        split('').
+        reverse().
+        join('');
+
+    return string == reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
