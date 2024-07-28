3a4937e3e4be22f6233ff39d59ca9e611ffdbf36
iokkuak <iokkuak@gmail.com>
2024-07-28 18:38:02 +0200

    exercise 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, current) => {
+    return sum + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mult, current) => {
+    return mult * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+  for(let i = 2; i <= n; i++){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
