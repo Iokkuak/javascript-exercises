58b06a504caf2c6609575477183e53bb63df03e9
iokkuak <iokkuak@gmail.com>
2024-07-28 19:24:41 +0200

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(typeof n !== 'number') {
+        n = parseInt(n);
+    }
 
+    if(n < 0) return "OOPS";
+    if(n == 0) return 0;
+
+    let curr = 1;
+    let prev = 0;
+
+    for(let i = 0; i < n-1; i++) {
+        const next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
