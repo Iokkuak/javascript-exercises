eb2366a005b3ab6b19c18d5238000d606575f4ea
iokkuak <iokkuak@gmail.com>
2024-07-29 09:22:57 +0200

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+function age(person) {
+    if(person.yearOfDeath === undefined) {
+        const currentYear = (new Date()).getFullYear() ; 
+        return currentYear - person.yearOfBirth; 
+    }
 
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+function compareAge(person1, person2) {
+    const age1 = age(person1);
+    const age2 = age(person2);
+    if(age1 > age2) {
+        return 1;
+    }
+    else if(age1 == age2) {
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+const findTheOldest = function(people) {
+    return people.
+        sort((person1, person2) => compareAge(person1, person2))[people.length-1]; 
 };
 
 // Do not edit below this line
